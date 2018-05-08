e205ed9f5defdb191f4659992f820d4911cd3a4a
Craig Colquitt <colquitt.craig@gmail.com>
2018-05-08 11:23:31 -0600

    Adding environment setup

---
+++ expo_mac_installs/install_scripts.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+##Here's is where install scripts or pointers to install scripts should be place.
+
+##ensure brew is installed
+if [ ! -f /usr/local/bin/brew ]; then
+    	echo "Brew not found here: /usr/local/bin/brew .. installing brew!"
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+##Run through Brew Installs
+brew update
+while read -r line || [[ -n "$line" ]]; do
+	if [[ "${line:0:1}" = *"#"* ]]; then
+		continue
+	fi
+	echo "Line from Brew inventory file is: $line"
+		a=( $line )
+		if [[ ${a[0]} = *"i"* ]]; then
+			echo "Brew installing ${a[1]}"
+			brew install ${a[1]}
+		elif [[ ${a[0]} = *"u"* ]]; then
+			echo "Brew UNinstalling ${a[1]}"
+			brew uninstall ${a[1]}
+		fi
+done < "$1"
+
+echo "Mac install scripts finished running";
+
+exit 0;
