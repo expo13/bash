cdf062074b2b7f4a16a81f18ee9b7c638ce9594e
Craig Colquitt <colquitt.craig@gmail.com>
2018-10-22 10:59:15 -0600

    more

--- setups/mac/install_scripts.sh
+++ setups/mac/install_scripts.sh
@@ -1,14 +1,41 @@
 #!/bin/bash
 
 ##Here's is where install scripts or pointers to install scripts should be place.
+##Only 1 argument should be supplied - the config file 
 
-#
-#
-#TODO WE MUST ADD THE CONFIG FILE CHECK HERE AND SET CONFIGS
-#
-#
+#============= TODO figure out if you can return a sgring value here
+function getConfig {
+
+while read -r line || [[ -n "$line" ]]; do
+	if [[ "${line:0:1}" = *"#"* ]]; then
+		continue
+	fi
+	echo "Line from config file is: $line"
+		param=`echo $line | cut -d':' -f 1`
+		if [ $param = $2 ]; then
+			value=`echo $line | cut -d':' -f 2`
+			echo "Setting config $param to value $value"
+			return "$value"
+		f
+done < "$1" #this is the file
+echo "No value found for config $2"
+exit 1
+}
+
+ANOTHERVALUE=`getConfig $1 $2`
+echo "here"
+exit 0
+#==============
 
 
+##input config file - only
+if [ -z "$1" ]; then
+    echo "No arguments homie!  Please list config file in arguments for setup: config file, bash profile, vimrc."
+fi
+if [ ! -f "$1" || ! -f "$2" || ! -f "$3" ]; then
+    echo "Argument not found. Arguments are: config file, bash profile, vimrc"
+    exit 1;
+fi
 
 ##ensure brew is installed
 if [ ! -f /usr/local/bin/brew ]; then
@@ -31,7 +58,7 @@ while read -r line || [[ -n "$line" ]]; do
 			echo "Brew UNinstalling ${a[1]}"
 			brew uninstall ${a[1]}
 		fi
-done < "$1"
+done < "$2" 
 
 ##BASH ===================================
 
@@ -46,10 +73,8 @@ echo "Touching vimrc file ... "
 touch ~/.vimrc
 
 ##Set ~/.vimrc file
-echo "Setting contents of vimrc file from $2"
-cat $2 > ~/.vimrc
-
-echo "Mac install scripts finished running";
+echo "Setting contents of vimrc file from $3"
+cat $3 > ~/.vimrc
 
 #Oh-my-git
 git clone https://github.com/arialdomartini/oh-my-git.git ~/.oh-my-git 
@@ -72,5 +97,30 @@ tar -xzvf google-cloud-sdk-220.0.0-darwin-x86_64.tar.gz
 ./google-cloud-sdk/install.sh
 ./google-cloud-sdk/bin/gcloud init
 
+#Local Email Server
+touch /etc/postfix/sasl_passwd
+echo "smtp.gmail.com:587 colquitt.craig@gmail.com:clobber-recap-peculiar" >> /etc/postfix/sasl_passwd
+postmap /etc/postfix/sasl_passwd
 
-exit 0;
+#safe to assume config file is legit because we have already validated at this point
+# first input param is config file, second is the desired config
+function getConfigValue {	
+while read -r line || [[ -n "$line" ]]; do
+	if [[ "${line:0:1}" = *"#"* ]]; then
+		continue
+	fi
+	echo "Line from config file is: $line"
+		a=( $line )
+		param = a | cut -d ':' -f 1
+		echo $param
+		value = a | cut -d ':' -f 2
+		echo $value
+		
+	echo 'someletters_12345_moreleters.ext' | cut -d'_' -f 2
+
+done < "$1" #this is the file
+}
+
+echo "Don't forget to install thunderbird email client by mozilla"
+
+exit 0
